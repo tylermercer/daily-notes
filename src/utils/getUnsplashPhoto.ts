d93b841ac83d926f6cd1a6b27be9ff31ad74d810
Tyler Mercer <acct@tylermercer.net>
2025-02-18 07:26:32 -0700

    Add filesystem cache for unsplash results
    And persist cache across GitHub Action runs

--- src/utils/getUnsplashPhoto.ts
+++ src/utils/getUnsplashPhoto.ts
@@ -1,13 +1,26 @@
+import { generateCacheKey, getCache } from "lib/cache/BufferCache";
+import type { Full } from "unsplash-js/dist/methods/photos/types";
 import unsplash from "./unsplash";
 
-export default async function getUnsplashPhoto(id: string) {
-    return await unsplash.photos
-        .get({ photoId: id })
-        .then((result) => {
-            if (result.errors) {
-                throw new Error(result.errors.join(", "));
-            } else {
-                return result.response;
-            }
-        });
+const cache = getCache('unsplash', './.cache/')
+
+export default async function getUnsplashPhoto(id: string): Promise<Full> {
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
+
+    const resultAsBuffer = await cache.getOrCreate(await generateCacheKey({ id }), async () => {
+        const result = await unsplash.photos
+            .get({ photoId: id })
+            .then((result) => {
+                if (result.errors) {
+                    throw new Error(result.errors.join(", "));
+                } else {
+                    return result.response;
+                }
+            });
+        const asArray = encoder.encode(JSON.stringify(result));
+        return Buffer.from(asArray.buffer);
+    });
+
+    return JSON.parse(decoder.decode(resultAsBuffer));
 }
