18ea2cf8682cea6e46298438a81fb8837ccf0f36
Tyler Mercer <acct@tylermercer.net>
2026-02-17 22:10:58 -0700

    Trying to fix build

--- src/utils/getUnsplashPhoto.ts
+++ src/utils/getUnsplashPhoto.ts
@@ -1,4 +1,4 @@
-import {getFileSystemStringCache } from "lib/cache/StringCache";
+import { getFileSystemStringCache } from "lib/cache/StringCache";
 import type { Full } from "unsplash-js/dist/methods/photos/types";
 import unsplash from "./unsplash";
 import { generateCacheKey } from "lib/cache/generateCacheKey";
@@ -6,17 +6,20 @@ import { generateCacheKey } from "lib/cache/generateCacheKey";
 const cache = getFileSystemStringCache('unsplash', './.cache/')
 
 export default async function getUnsplashPhoto(unsplashUrl: string): Promise<Full> {
-    const id =  unsplashUrl.slice(-11);
+    const id = unsplashUrl.slice(-11);
     return JSON.parse(await cache.getOrCreate(await generateCacheKey({ id }), async () => {
         const result = await unsplash.photos
             .get({ photoId: id })
             .then((result) => {
                 if (result.errors) {
-                    throw new Error(result.errors.join(", "));
+                    throw new Error(`Error getting ${id}: ${result.errors.join(", ")}`);
                 } else {
                     return result.response;
                 }
-            });
+            })
+            .catch((e: Error) => {
+                throw new Error(`Error getting ${id} (client): ${e.message}`);
+            })
         return JSON.stringify(result);
     }));
 }
